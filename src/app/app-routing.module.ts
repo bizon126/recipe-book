e996384a0e9eee963b86c7b3583b1e513277adeb
Oleg Agapitov <abusabir@ya.ru>
2024-02-06 11:28:12 +0500

    Resolver for recipe-detail added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {RecipesComponent} from "./recipes/recipes.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
 import {RecipesStartPageComponent} from "./recipes/recipes-start-page/recipes-start-page.component";
 import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
+import {RecipesResolverService} from "./recipes/recipes-resolver.service";
 
 const appRoutes:Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: "full"},
   { path: 'recipes', component: RecipesComponent, children: [
       { path: '', component: RecipesStartPageComponent},
       { path: 'new', component: RecipeEditComponent},
-      { path: ':id', component: RecipeDetailComponent},
-      { path: ':id/edit', component: RecipeEditComponent}
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
 
     ] },
   { path: 'shopping-list', component: ShoppingListComponent}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {map} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -19,18 +19,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://recipe-book-d55bd-default-rtdb.firebaseio.com/recipes.json')
       .pipe(
         map(recipes => {
           return recipes.map(recipe => {
             return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
           })
+        }),
+        tap(recipes => {
+          this.recipesService.setRecipes(recipes);
         })
-      )
-      .subscribe(recipes => {
-        this.recipesService.setRecipes(recipes);
-      })
+      );
   }
 
 }
